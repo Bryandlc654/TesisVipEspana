8bdc7b96104b9be373d22d47838ff4accd42a1c1
Bryandlc654 <bdelacruz654@gmail.com>
2024-09-19 22:56:41 -0500

    Correccion de audios

--- src/components/home/CarrouselClient.jsx
+++ src/components/home/CarrouselClient.jsx
@@ -14,6 +14,81 @@ import Audio5 from "/public/audios/5.mp3"
 import { Navigation, Pagination, Mousewheel, Keyboard, Autoplay } from 'swiper/modules';
 
 export default function CarrouselClient() {
+  const handlePlayClick1 = () => {
+      const audio = document.getElementById("audio1");
+      const btn = document.getElementById("btn1");
+
+      audio.setAttribute("controls", true);
+      audio.play();
+
+      btn.style.display = "none";
+
+      audio.onpause = function() {
+        btn.style.display = "flex";
+        audio.removeAttribute("controls");
+      };
+  };
+
+  const handlePlayClick2 = () => {
+    const audio = document.getElementById("audio2");
+    const btn = document.getElementById("btn2");
+
+    audio.setAttribute("controls", true);
+    audio.play();
+
+    btn.style.display = "none";
+
+    audio.onpause = function() {
+      btn.style.display = "flex";
+      audio.removeAttribute("controls");
+    };
+  };
+
+  const handlePlayClick3 = () => {
+    const audio = document.getElementById("audio3");
+    const btn = document.getElementById("btn3");
+
+    audio.setAttribute("controls", true);
+    audio.play();
+
+    btn.style.display = "none";
+
+    audio.onpause = function() {
+      btn.style.display = "flex";
+      audio.removeAttribute("controls");
+    };
+  };
+
+  const handlePlayClick4 = () => {
+    const audio = document.getElementById("audio4");
+    const btn = document.getElementById("btn4");
+
+    audio.setAttribute("controls", true);
+    audio.play();
+
+    btn.style.display = "none";
+
+    audio.onpause = function() {
+      btn.style.display = "flex";
+      audio.removeAttribute("controls");
+    };
+  };
+
+  const handlePlayClick5 = () => {
+    const audio = document.getElementById("audio5");
+    const btn = document.getElementById("btn5");
+
+    audio.setAttribute("controls", true);
+    audio.play();
+
+    btn.style.display = "none";
+
+    audio.onpause = function() {
+      btn.style.display = "flex";
+      audio.removeAttribute("controls");
+    };
+  };
+
   const [slidesPerView, setSlidesPerView] = useState(3);
 
   useEffect(() => {
@@ -52,31 +127,56 @@ export default function CarrouselClient() {
         <SwiperSlide>
           <article className="opinion">
             <p className="opinion__title paragraph">Redacción de Trabajos de Fin de Master</p>
-            <audio className="opinion__audio" src={Audio1} controls></audio>
+            <audio id="audio1" className="opinion__audio" src={Audio1}></audio>
+            <button id="btn1" onClick={handlePlayClick1} className="opinion__btn-audio" title="Reproducir audio">
+                <svg className="video__btn-icon" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393" />
+                </svg>
+            </button>
           </article>
         </SwiperSlide>
         <SwiperSlide>
         <article className="opinion">
             <p className="opinion__title paragraph">Redacción de Trabajos de Fin de Grado</p>
-            <audio className="opinion__audio" src={Audio2} controls></audio>
+            <audio id="audio2" className="opinion__audio" src={Audio2}></audio>
+            <button id="btn2" onClick={handlePlayClick2} className="opinion__btn-audio" title="Reproducir audio">
+                <svg className="video__btn-icon" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393" />
+                </svg>
+            </button>
           </article>
         </SwiperSlide>
         <SwiperSlide>
         <article className="opinion">
             <p className="opinion__title paragraph">Redacción de Trabajos de Fin de Grado</p>
-            <audio className="opinion__audio" src={Audio3} controls></audio>
+            <audio id="audio3" className="opinion__audio" src={Audio3}></audio>
+            <button id="btn3" onClick={handlePlayClick3} className="opinion__btn-audio" title="Reproducir audio">
+                <svg className="video__btn-icon" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393" />
+                </svg>
+            </button>
           </article>
         </SwiperSlide>
         <SwiperSlide>
         <article className="opinion">
             <p className="opinion__title paragraph">Redacción de Trabajos de Fin de Master</p>
-            <audio className="opinion__audio" src={Audio4} controls></audio>
+            <audio id="audio4" className="opinion__audio" src={Audio4}></audio>
+            <button id="btn4" onClick={handlePlayClick4} className="opinion__btn-audio" title="Reproducir audio">
+                <svg className="video__btn-icon" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393" />
+                </svg>
+            </button>
           </article>
         </SwiperSlide>
         <SwiperSlide>
         <article className="opinion">
             <p className="opinion__title paragraph">Redacción de Trabajos de Fin de Master</p>
-            <audio className="opinion__audio" src={Audio5} controls></audio>
+            <audio id="audio5" className="opinion__audio" src={Audio5}></audio>
+            <button id="btn5" onClick={handlePlayClick5} className="opinion__btn-audio" title="Reproducir audio">
+                <svg className="video__btn-icon" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393" />
+                </svg>
+            </button>
           </article>
         </SwiperSlide>
       </Swiper>
